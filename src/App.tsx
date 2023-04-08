13339e7a9cabfa2506d3c8e2dbda2c26878eb9f1
Gabriel Petersson <gabriielpetersson@gmail.com>
2023-04-08 12:43:10 -0700

    editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import "./App.css";
 import { TextEditor } from "./Editor";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <h1>Vite + React</h1>
