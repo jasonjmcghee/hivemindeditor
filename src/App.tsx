055a3babc950b21fa8a868bcf20ca4c75fb97e6a
Gabriel Petersson <gabriielpetersson@gmail.com>
2023-04-08 12:42:56 -0700

    editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import { TextEditor } from "./Editor";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -7,17 +8,7 @@ function App() {
   return (
     <div className="App">
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <TextEditor />
     </div>
   );
 }
--- src/Editor.tsx
+++ src/Editor.tsx
@@ -10,7 +10,7 @@ import SubScript from "@tiptap/extension-subscript";
 const content =
   '<h2 style="text-align: center;">Welcome to Mantine rich text editor</h2><p><code>RichTextEditor</code> component focuses on usability and is designed to be as simple as possible to bring a familiar editing experience to regular users. <code>RichTextEditor</code> is based on <a href="https://tiptap.dev/" rel="noopener noreferrer" target="_blank">Tiptap.dev</a> and supports all of its features:</p><ul><li>General text formatting: <strong>bold</strong>, <em>italic</em>, <u>underline</u>, <s>strike-through</s> </li><li>Headings (h1-h6)</li><li>Sub and super scripts (<sup>&lt;sup /&gt;</sup> and <sub>&lt;sub /&gt;</sub> tags)</li><li>Ordered and bullet lists</li><li>Text align&nbsp;</li><li>And all <a href="https://tiptap.dev/extensions" target="_blank" rel="noopener noreferrer">other extensions</a></li></ul>';
 
-export const Editor = () => {
+export const TextEditor = () => {
   const editor = useEditor({
     extensions: [
       StarterKit,
